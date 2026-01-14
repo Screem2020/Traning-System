77849e6af41337f11f9f42cf19e384e7916d6d67
Mihael <Churin_s@inbox.ru>
2026-01-14 19:22:30 +0200

    test GETcontroller in  'teacherContollerTest'

--- src/test/java/com/example/traningsystem/controller/TeacherControllerTest.java
+++ src/test/java/com/example/traningsystem/controller/TeacherControllerTest.java
@@ -1,14 +1,39 @@
 package com.example.traningsystem.controller;
 
+import com.example.traningsystem.dao.TeacherRepository;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
 class TeacherControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SneakyThrows
     @Test
     void findAll() {
+        mockMvc.perform(get("/api/v1/teacher"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$.content").isArray())
+                .andExpect(jsonPath("$.content").isEmpty())
+                .andReturn();
     }
 
     @Test
