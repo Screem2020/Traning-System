aff6d5f0979354a5a08dedf7fbe620f1175a8226
Mihael <Churin_s@inbox.ru>
2026-01-16 12:47:38 +0200

    test 'save'in' teacherController'

--- src/test/java/com/example/traningsystem/controller/TeacherControllerTest.java
+++ src/test/java/com/example/traningsystem/controller/TeacherControllerTest.java
@@ -1,18 +1,16 @@
 package com.example.traningsystem.controller;
 
-import com.example.traningsystem.dao.TeacherRepository;
+import com.example.traningsystem.dao.CourseRepository;
+import com.example.traningsystem.model.CourseEntity;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.MediaType;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,10 +22,14 @@ class TeacherControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private CourseRepository courseRepository;
 
     @SneakyThrows
     @Test
     void findAll() {
+
+
         mockMvc.perform(get("/api/v1/teacher"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
@@ -36,8 +38,29 @@ class TeacherControllerTest {
                 .andReturn();
     }
 
+    @SneakyThrows
     @Test
     void save() {
+        CourseEntity courseEntity = new CourseEntity();
+        courseEntity.setCourseName("Java");
+        courseEntity.setDescription("Java the Best");
+        courseRepository.save(courseEntity);
+
+        String teacherJson = """
+                {
+                "firstName":"Nina",
+                "lastName":"Alekseevna",
+                "courseId":%d
+                }
+                """.formatted(courseEntity.getId());
+
+        mockMvc.perform(post("/api/v1/teacher")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(teacherJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value("Nina"))
+                .andExpect(jsonPath("$.lastName").value("Alekseevna"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
     }
 
     @Test
